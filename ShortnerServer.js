8fce63864744fda520a2aa53d535733f72937765
Typecaster12 <harshmishra2997@gmail.com>
2026-01-12 23:41:27 +0530

    backend is completed and connected with frontend

--- ShortnerServer.js
+++ ShortnerServer.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const queryString = require('querystring');
+const { log } = require('console');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
@@ -16,15 +17,15 @@ const server = http.createServer((req, res) => {
             res.write(data);
             res.end();
         });
-    } if (req.url === '/submit' && req.method === 'POST') {
+    } else if (req.url === '/submit' && req.method === 'POST') {
         //will store the data in normal text first, then use this as useable formate;
         let textData = "";
-
         req.on('data', (chunk) => {
             textData += chunk.toString();
         });
 
         req.on('end', () => {
+            //object containing mainUrl and alias;
             let dataObject = queryString.parse(textData);
 
             //for alias formate validation
@@ -35,6 +36,7 @@ const server = http.createServer((req, res) => {
             let alias = dataObject.alias;
 
             //into proper object formate;
+            //!currently not in use..
             let finalDataObject = {
                 mainUrl,
                 alias // <!--“Alias” means: a chosen nickname that points to something real-->
@@ -57,15 +59,23 @@ const server = http.createServer((req, res) => {
 
             let data = {};
 
+            //if file is already there?
             if (fs.existsSync('storage.json')) {
+                //this contains the data(object) from json file;
                 const fileContent = fs.readFileSync('storage.json', 'utf-8').trim();
-
+                console.log("fileContent is : " + fileContent);//for debug;
+                //if fileContent have some data, parse that data into json data(make it json);
                 if (fileContent) {
                     data = JSON.parse(fileContent);
+                    // console.log("Logged data : " + data);//for debug;
                 }
             }
 
+            //contains actual json data;
+            console.log(data);
+
             //for uniqueness of alias
+            // console.log("data[alias]" + data[alias]);
             if (data[alias]) {
                 res.writeHead(409, { 'Content-Type': 'text/plain' });
                 res.end("Alias already exists. Choose another one.");
@@ -84,9 +94,9 @@ const server = http.createServer((req, res) => {
 
             //merg the data now,
             data[alias] = {
-                url: mainUrl,
-                timeStamp: new Date().toISOString(),
-                clicks: 0
+                url: mainUrl, //the url of that alias;
+                timeStamp: new Date().toISOString(), //timeStamp;
+                clicks: 0 //no. of times we make request with same alias;
             }
 
             //finally making json data out of final data we get,
@@ -95,6 +105,43 @@ const server = http.createServer((req, res) => {
             res.writeHead(201, { 'Content-Type': 'text/plain' });
             res.end(`Short link created: /${alias}`);
         });
+    } else {
+        //now the final part, we have to make Get request here so that use can get the response;
+        const getAlias = req.url.slice(1);//as our url have /ouralias => se we will get only alias and (/) is removed;
+
+        let getData = {};
+        //reading the json file to check if we have alias present in our storage file(json);
+        //but first check if we have file
+        //!we have done this process before but as this is for get request, 
+        if (fs.existsSync('Storage.json')) {
+            //read
+            const getFileContent = fs.readFileSync('storage.json', 'utf-8').trim();
+
+            if (getFileContent) {
+                getData = JSON.parse(getFileContent);
+            }
+        }
+
+        //if we have same alais already present there then we have to update the number of clicks;
+        if (getData[getAlias]) {
+            //clicks update;
+            getData[getAlias].clicks += 1;
+
+            //update the changes to the file;
+            fs.writeFileSync('storage.json', JSON.stringify(getData, null, 2));
+
+            //rediricting the user with 302 status code;
+            res.writeHead(302, {
+                location: getData[getAlias].url,
+            });
+
+            console.log(req.url);
+            //ending the response;
+            res.end(`Short link created: /${alias}`);
+        } else {
+            res.writeHead(400, { 'content-type': 'text/plain' });
+            res.end("Short link not found");
+        }
     }
 });
 
